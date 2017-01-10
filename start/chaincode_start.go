c86d8a7ed4be9804903e3ad780eb3f49b3b301d0
dbourasseau <dbourasseau@viotech.net>
2017-01-10 17:16:24 +0100

    fix error

--- start/chaincode_start.go
+++ start/chaincode_start.go
@@ -110,7 +110,7 @@ func (t *SimpleChaincode) read(stub shim.ChaincodeStubInterface, args []string) 
 	var err error
 	fmt.Println("Runing read")
 	if len(args) != 1 {
-		errors.New("Incorrect number of arguments. Expecting name of the key to query")
+		nil, errors.New("Incorrect number of arguments. Expecting name of the key to query")
 	}
 	key = args[0]
 	valAsbytes, err := stub.GetState(key)
@@ -118,6 +118,6 @@ func (t *SimpleChaincode) read(stub shim.ChaincodeStubInterface, args []string) 
 		jsonResp = "{\"Error\":\"Failed to get state for " + key + "\"}"
 		return nil, errors.New(jsonResp)
 	}
-	fmt.Println("write retrun " + string(valAsbytes))
+	//fmt.Println("write retrun " + string(valAsbytes))
 	return valAsbytes, nil
 }
